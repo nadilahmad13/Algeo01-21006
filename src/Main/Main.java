344b68993ec3296d844291a6b661076487bf898d
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-09-27 11:00:58 +0700

    Updated Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 import Matrix.Matrix;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +8,14 @@ public class Main {
         double[][] matrix = Matrix.InputMatrix();
         // output the matrix
         Matrix.outputMatrix(matrix);
+        // gauss
+        Matrix.Gauss(matrix);
+        // output the matrix
+        System.out.println("Hasil eliminasi Gauss:");
+        Matrix.outputMatrix(matrix);
+
+        // calculate determinant
+        double det = Matrix.DetByGauss(matrix);
+        System.out.println("Determinan matriks: " + det);
     }
 }
