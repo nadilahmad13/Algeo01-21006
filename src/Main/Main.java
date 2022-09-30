047917dbf668da92e75815dc0e2e8a67e66762c2
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-09-30 10:59:53 +0700

    Updated misc

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,23 +4,12 @@ import Functions.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(
-            "Menu"
-        );
-        // read a matrix
-        double[][] matrix = Input_Matrix.MatrixKeyboardInput();
-        double[][] matrixx = Input_Matrix.MatrixKeyboardInput();
-        // output the matrix
-        Matrix.outputMatrix(matrixx);
-        // det by kofaktor
-        double det = Matrix.DetByGauss(matrix);
-        System.out.println("Determinan = " + det);
-        // transpose
-        System.out.println("Transpose :");
-        double[][] transpose = Matrix.TransposeMatrix(matrix);
-        Matrix.outputMatrix(transpose);
-
-        //cramer
-
+        double[][] M = Input_Matrix.MatrixFileInput();
+        double[][] M2 = Matrix.CopyMatrix(M);
+        M = Inverse.InverseByCofactor(M);
+        M2 = Inverse.InverseByOBE(M);
+        Matrix.outputMatrix(M);
+        System.out.println("");
+        Matrix.outputMatrix(M2);
     }
 }
